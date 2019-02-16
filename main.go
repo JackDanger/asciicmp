22d9acc3c2b635cfb3eb95ffc511c5768f348ac6
Jack Danger <github@jackcanty.com>
2019-02-16 12:31:30 -0800

    usage

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"io"
-  "fmt"
+	"fmt"
 	"log"
-	"net/http"
 	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/examples/util"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
-	"github.com/google/gopacket/tcpassembly"
-	"github.com/google/gopacket/tcpassembly/tcpreader"
 )
 
 var iface = flag.String("i", "lo0", "Interface to get packets from")
@@ -22,43 +17,9 @@ var logAllPackets = flag.Bool("v", false, "Logs every packet in great detail")
 
 // Disable kernel ICMP responses with this:
 // sudo sh -c 'echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_all'
-
-type httpStreamFactory struct{}
-
-// httpStream will handle the actual decoding of http requests.
-type httpStream struct {
-	net, transport gopacket.Flow
-	r              tcpreader.ReaderStream
-}
-
-func (h *httpStreamFactory) New(net, transport gopacket.Flow) tcpassembly.Stream {
-	hstream := &httpStream{
-		net:       net,
-		transport: transport,
-		r:         tcpreader.NewReaderStream(),
-	}
-	go hstream.run() // Important... we must guarantee that data from the reader stream is read.
-
-	// ReaderStream implements tcpassembly.Stream, so we can return a pointer to it.
-	return &hstream.r
-}
-
-func (h *httpStream) run() {
-	buf := bufio.NewReader(&h.r)
-	for {
-		req, err := http.ReadRequest(buf)
-		if err == io.EOF {
-			// We must read until we see an EOF... very important!
-			return
-		} else if err != nil {
-			log.Println("Error reading stream", h.net, h.transport, ":", err)
-		} else {
-			bodyBytes := tcpreader.DiscardBytesToEOF(req.Body)
-			req.Body.Close()
-			log.Println("Received request from stream", h.net, h.transport, ":", req, "with", bodyBytes, "bytes in request body")
-		}
-	}
-}
+//
+// send pings on BSD/OS X with a specified outbound ip
+// ping yahoo.com -S $(ipconfig getifaddr en0) -c 1
 
 func main() {
 	defer util.Run()()
@@ -66,9 +27,9 @@ func main() {
 	var err error
 
 	// Set up pcap packet capture
-  log.Printf("Starting capture on interface %q", *iface)
-  const snapLen int32 = 1600
-  handle, err = pcap.OpenLive(*iface, snapLen, true, pcap.BlockForever)
+	log.Printf("Starting capture on interface %q", *iface)
+	const snapLen int32 = 1600
+	handle, err = pcap.OpenLive(*iface, snapLen, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -76,11 +37,6 @@ func main() {
 	if err := handle.SetBPFFilter("icmp"); err != nil {
 		log.Fatal(err)
 	}
-
-	// Set up assembly
-	streamFactory := &httpStreamFactory{}
-	streamPool := tcpassembly.NewStreamPool(streamFactory)
-	assembler := tcpassembly.NewAssembler(streamPool)
 
 	log.Println("reading in packets")
 	// Read in packets, pass to assembler.
@@ -97,22 +53,21 @@ func main() {
 			if *logAllPackets {
 				log.Println(packet)
 			}
-fmt.Printf("LayerType %#v\n", packet.NetworkLayer().LayerType())
-fmt.Printf("NetworkLayer %#v\n", packet.NetworkLayer())
-fmt.Printf("TransportLayer %#v\n", packet.TransportLayer())
+			fmt.Printf("LayerType %#v\n", packet.NetworkLayer().LayerType())
+			fmt.Printf("NetworkLayer %#v\n", packet.NetworkLayer())
+			fmt.Printf("TransportLayer %#v\n", packet.TransportLayer())
 			if packet.NetworkLayer() == nil || packet.TransportLayer() == nil || packet.TransportLayer().LayerType() != layers.LayerTypeTCP {
 				log.Println("Unusable packet")
-        log.Println(packet)
-fmt.Printf("WHOLE PACKET main.go:103 %#v\n", packet)
+				log.Println(packet)
+				fmt.Printf("WHOLE PACKET main.go:103 %#v\n", packet)
 				continue
 			}
 			tcp := packet.TransportLayer().(*layers.TCP)
-			assembler.AssembleWithTimestamp(packet.NetworkLayer().NetworkFlow(), tcp, packet.Metadata().Timestamp)
+			fmt.Printf("main.go:68 %#v\n", tcp)
 
 		case <-ticker:
 			// Every minute, flush connections that haven't seen activity in the past 2 minutes.
-			assembler.FlushOlderThan(time.Now().Add(time.Minute * -2))
+			fmt.Printf(".")
 		}
 	}
 }
-
